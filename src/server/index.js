55abf8e5b43570bd36dbe9d16613514cb92f40e8
Christian Snodgrass <csnodgrass3147@gmail.com>
2019-09-17 21:37:19 -0400

    Updated public copyfiles path

--- src/server/index.js
+++ src/server/index.js
@@ -15,6 +15,9 @@ const app = express();
   app.use('/client', express.static(join(__dirname, '../client')));
   app.use('/public', express.static(join(__dirname, '../public')));
 
+  app.get('/client/**', (_, res) => res.status(404).end('404 Not Found'));
+  app.get('/public/**', (_, res) => res.status(404).send('404 Not Found'));
+
   app.get('/favicon.ico', (_, res) => {
     res.status(204).header("Expires", (new Date(Date.now() + 1000 * 60 * 60 * 24 * 365))
         .toUTCString()).end();
